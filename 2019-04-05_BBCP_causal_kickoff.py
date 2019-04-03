6a81d87e7595f7a4ccd24b3d95a74063ecf9f665
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-04-03 11:39:47 +0200

    updating slides

--- 2019-04-05_BBCP_causal_kickoff.py
+++ 2019-04-05_BBCP_causal_kickoff.py
@@ -212,8 +212,9 @@ def create_movie(seq, T=.5, radius=1/64, fps=50, W=1000, H=600, figname='sequenc
             rect = gz.rectangle(xy=(x, H/2.), fill=(1, 0, 0), **s)
             rect.draw(surface)
 
-        text = gz.text(f"{i:02d}/{len(seq):02d}", fontfamily="Impact",  fontsize=W/25,
-                          fill=(1,1,1), xy=(W/10, H/10), angle=0)
+        text = gz.text(f"{i+1:02d}/{len(seq):02d}", fontfamily="Impact",  fontsize=W/25,
+                            h_align="left", v_align="top",
+                          fill=(1,1,1), xy=(W/20, H/20), angle=0)
         text.draw(surface)
         # print (surface.get_npimage().shape)
         return surface.get_npimage()
